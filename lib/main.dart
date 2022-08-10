026406e49de7c1fbc1e6766f6e091253f7c321aa
sinan <sinanalagoz74@gmail.com>
2022-08-10 20:48:12 +0300

    765y

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
@@ -5,10 +8,11 @@ void main() {
   runApp(const MyApp());
 }
 
+enum Animvalues { forward, backward }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,53 +33,139 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
- 
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  // late Animation anime;
+
+  // void animation_backward() {
+  //   animationController.reset();
+  //   animationController.value = 1;
+  //   animationController.reverse();
+
+  //   animationController.addListener(() {
+  //     if (animationController.isCompleted) {
+  //       animation_forward();
+  //     }
+  //     //  print('${animationController.value}');
+  //     setState(() {});
+  //   });
+  // }
+
+  // void animation_backward() {
+  //   animationController.reset();
+  //   animationController.value = 1;
+  //   animationController.duration = const Duration(seconds: 5);
+  //   animationController.reverse();
+
+  //   animationController.addListener(() {
+  //     if (animationController.isCompleted) {
+  //       print("backward completed");
+  //       animationController.value = 0;
+  //       animation_forward();
+  //       // animation_backward();
+  //     }
+  //     //  print('${animationController.value}');
+  //     setState(() {});
+  //   });
+  // }
+
+  void animation_forward(Animvalues a_values) {
+    animationController.reset();
+    animationController.duration = const Duration(seconds: 3);
+
+    animationController.value = 0;
+
+    animationController.forward();
+
+    animationController.addListener(() {
+      if (animationController.isCompleted) {
+        print("forward completed");
+        animationController.reverse();
+      }
+      if (animationController.isDismissed) {
+        print("forward completed");
+        animationController.forward();
+      }
+      //  print('${animationController.value}');
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      value: 0,
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    // anime =
+    //     CurvedAnimation(parent: animationController, curve: Curves.elasticIn);
+
+    super.initState();
+  }
+
+
+String rnd()
+{
+  String rnd1="";
+  for(int i=0; i<10000 ;i++) {
+    rnd1 += Random().nextInt(9).toString();
+  }
+  return rnd1;
+}
+
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Stack(
-    
-         
           children: <Widget>[
-            
-              Text("0"*10000, style: const TextStyle(color: Colors.lightGreen),),
-              Center(
-                child: ClipRect(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                      sigmaX: 2.0,
-                      sigmaY: 2.0
-              
-                    ),
-                    // ignore: sized_box_for_whitespace
-                    child:  Container(
-                      width: 250,
-                      height: 250,
-                      child: const Center(child:  Text("C-NON Software",
-                      style: TextStyle(fontSize: 25,
-                      color: Colors.lightGreenAccent,
-                      fontWeight: FontWeight.bold
-                      ),
-                      )
-                      ),
-                    ),
-                  
+            Text(
+              // "${new Random().}" * 10000,
+              rnd(),
+              style: const TextStyle(color: Colors.lightGreen),
+            ),
+            Center(
+              child: ClipRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                      sigmaX: animationController.value * 4, //2.0,
+
+                      sigmaY: 1.0),
+                  // ignore: sized_box_for_whitespace
+                  child: Container(
+                    width: 250,
+                    height: 250,
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              animation_forward(Animvalues.forward);
+                            },
+                            child: const Text("Press")),
+                        const Text(
+                          "C-NON Software",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.lightGreenAccent,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )),
                   ),
                 ),
               ),
-
+            ),
           ],
         ),
       ),
-      
     );
   }
 }
